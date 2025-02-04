be39c6e45c2be0b12c9cf23615705f4c22a57297
Nandan <nandanr7284@gmail.com>
2025-02-04 23:57:32 +0530

    fullstack chat application

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://fullstack-chat-application-hc4x.onrender.com",
   credentials: true,
 }));
 app.use(express.json());
